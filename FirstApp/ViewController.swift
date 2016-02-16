264cc0d60ef36317d1055c1f37e27c59b57d13fd
Zeshan Anwar <m.zeshan.a93@gmail.com>
2016-02-15 20:28:48 -0500

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Zeshan Anwar on 2016-02-15.
+//  Copyright © 2016 Zeshan Anwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
